3ab7b7f2c05888c4211b2c1365ec73590efc6bdd
scrummer <scrummer@gmx.ch>
2025-10-17 13:01:08 +0200

    Refactor tests to remove version checks and set correct value for `Version::MAJOR_VERSION`

--- tests/Model/DataType/LocalizedFieldTest.php
+++ tests/Model/DataType/LocalizedFieldTest.php
@@ -36,23 +36,14 @@ class LocalizedFieldTest extends ModelTestCase
     {
         parent::setUp();
 
-        if (Version::getMajorVersion() >= 11) {
-            $openDxpModule = $this->getModule('\\'.OpenDxp::class);
-            $this->config = $openDxpModule->grabService(SystemSettingsConfig::class);
-            $this->originalConfig = $this->config->get();
-        } else {
-            $this->originalConfig = \OpenDxp\Config::getSystemConfiguration();
-        }
-
+        $openDxpModule = $this->getModule('\\'.OpenDxp::class);
+        $this->config = $openDxpModule->grabService(SystemSettingsConfig::class);
+        $this->originalConfig = $this->config::get();
     }
 
     public function tearDown(): void
     {
-        if (Version::getMajorVersion() >= 11) {
-            $this->config->testSave($this->originalConfig);
-        } else {
-            \OpenDxp\Config::setSystemConfiguration($this->originalConfig);
-        }
+        $this->config->testSave($this->originalConfig);
 
         Localizedfield::setStrictMode((bool)Localizedfield::STRICT_DISABLED);
     }
@@ -127,11 +118,7 @@ class LocalizedFieldTest extends ModelTestCase
         $configuration = $this->originalConfig;
         $configuration['general']['fallback_languages']['de'] = 'en';
 
-        if (Version::getMajorVersion() >= 11) {
-            $this->config->testSave($configuration);
-        } else {
-            \OpenDxp\Config::setSystemConfiguration($configuration);
-        }
+        $this->config->testSave($configuration);
 
         $object = TestHelper::createEmptyObject();
 
--- tests/Model/Inheritance/LocalizedFieldTest.php
+++ tests/Model/Inheritance/LocalizedFieldTest.php
@@ -39,23 +39,14 @@ class LocalizedFieldTest extends ModelTestCase
         TestHelper::cleanUp();
         \OpenDxp::setAdminMode();
 
-        if (Version::getMajorVersion() >= 11) {
-            $openDxpModule = $this->getModule('\\'.OpenDxp::class);
-            $this->config = $openDxpModule->grabService(SystemSettingsConfig::class);
-            $this->originalConfig = $this->config->get();
-        } else {
-            $this->originalConfig = \OpenDxp\Config::getSystemConfiguration();
-        }
-
+        $openDxpModule = $this->getModule('\\'.OpenDxp::class);
+        $this->config = $openDxpModule->grabService(SystemSettingsConfig::class);
+        $this->originalConfig = $this->config::get();
     }
 
     public function tearDown(): void
     {
-        if (Version::getMajorVersion() >= 11) {
-            $this->config->testSave($this->originalConfig);
-        } else {
-            \OpenDxp\Config::setSystemConfiguration($this->originalConfig);
-        }
+        $this->config->testSave($this->originalConfig);
 
         parent::tearDown();
     }
@@ -65,11 +56,8 @@ class LocalizedFieldTest extends ModelTestCase
         $configuration = $this->originalConfig;
         $configuration['general']['fallback_languages']['de'] = 'en';
 
-        if (Version::getMajorVersion() >= 11) {
-            $this->config->testSave($configuration);
-        } else {
-            \OpenDxp\Config::setSystemConfiguration($configuration);
-        }
+        $this->config->testSave($configuration);
+
         // create root -> one -> two -> three
         $one = new Inheritance();
         $one->setKey('one');
